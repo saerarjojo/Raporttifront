a6c744d19f0a4f22806d793e1fe8986c8005df38
ArtturiP <89945435+ArtturiP@users.noreply.github.com>
2021-11-30 15:05:53 +0200

    ensimmäinen versio tulosraportille

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Kyselylist from './components/Kyselylist';
+import Tulosraportti from './components/Tulosraportti';
+
+function App()
+{
+  return (
+    <div className="App">
+      <Tulosraportti />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Tulosraportti.js
@@ -0,0 +1,83 @@
+import React from 'react';
+
+function Tulosraportti()
+{
+
+    const [id, setId] = React.useState('');
+
+    const [title, setTitle] = React.useState('');
+
+    const [done, setDone] = React.useState(false);
+
+    var questions = [];
+    var answer;
+    var qSize = 0;
+
+    React.useEffect(() => {    
+
+        fetchAnswers();
+
+    }, )
+
+    function fetchAnswers()
+    {
+    fetch('http://localhost:8080/rest/kyselyt/' + id)
+    .then(response => response.json())
+    .then(data => {
+
+        qSize = data.kysymykset.length;
+
+        if(!done)
+        {   
+            setDone(true);
+
+            for(var i=0; i < qSize; i++)
+            {
+                setTitle(data.title);
+
+                questions[i] = data.kysymykset[i].kysymys;
+
+                document.getElementById("questions").innerHTML += 
+                    "Kysymys " + (i + 1) + " : " + questions[i] + "<br/>Vastaus : ";
+
+                
+                if (data.kysymykset[i].vastaukset[0] != null) {
+                    answer = data.kysymykset[i].vastaukset[0].vastaus; 
+                } else {
+                    answer = "Ei vastattu";
+                }
+
+                console.log(answer);
+                
+                document.getElementById("questions").innerHTML +=
+                    answer + "<br/><br/>";  
+                
+                
+            }
+
+
+
+        }
+    })
+
+        .catch(err => console.log(err))
+
+}
+return(
+    <div>
+        <h3>Tulosraportti</h3>
+
+        <form>
+            <input value={id} onChange={event => setId(event.target.value)}/>
+        </form>
+
+        <p>Title: {title}</p>
+        <div id="questions"></div>
+
+
+
+    </div>
+    )
+}
+
+export default Tulosraportti;
